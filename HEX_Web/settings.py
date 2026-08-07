76b4dd5c7be986043979d236b14c34b7114138c6
Atticus Walton <139408126+ExcessMarrow773@users.noreply.github.com>
2026-08-06 23:31:29 -0500

    adds staticfiles

--- HEX_Web/settings.py
+++ HEX_Web/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -109,14 +111,19 @@ TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    f'{BASE_DIR}/static',
+]
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
