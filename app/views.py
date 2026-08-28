4ba8223fe3e35a6d4a23968439fdb0a5e5a73956
Atticus Walton <atticus.falkner.walton@gmail.com>
2026-08-28 22:19:58 +0000

    starts working on the staff page

--- app/views.py
+++ app/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, "app/index.html", context)
+
+def staff(request):
+    context = {}
+    return render(request, "app/staff.html", context)
