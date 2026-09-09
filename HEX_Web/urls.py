a2ec1640a99bd70902f14a200ad05d0889d81b71
Atticus Walton <atticus.falkner.walton@gmail.com>
2026-09-09 19:10:15 +0000

    adds namespacing

--- HEX_Web/urls.py
+++ HEX_Web/urls.py
@@ -20,9 +20,10 @@ from accounts.views import CustomLoginView, CustomLogoutView
 from django.conf import settings
 from django.conf.urls.static import static
 
+app_name = 'base'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', include('app.urls')),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
