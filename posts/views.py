08c22aeab8900b84ebc07e09faed8237de744d45
Atticus Walton <atticus.falkner.walton@gmail.com>
2026-09-11 13:59:23 +0000

    adds imports

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+
+from posts.forms import CreatePost
+from posts.models import Post
+
+from accounts.models import CustomUser
+
+User = get_user_model()
 # Create your views here.
 
 # @login_required
